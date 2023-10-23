5cc87ffac73a564d6a671f288ab33b74e81d36be
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-23 14:34:31 +0300

    Initial commit, refactor PostRepositoryStubImpl, request.http

--- src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
+++ src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -1,6 +1,12 @@
 package ru.netology.repository;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
@@ -12,28 +18,49 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class PostRepositoryStubImpl implements PostRepository {
+
     private final AtomicLong postId;
     private final Map<Long, Post> postsMap;
+    private static final String REMOVED = "removed";
 
     public PostRepositoryStubImpl() {
         postId = new AtomicLong(0);
         postsMap = new ConcurrentHashMap<>();
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class NotFoundException extends RuntimeException {
+    }
+
     public List<Post> all() {
-        return new ArrayList<>(postsMap.values());
+        List<Post> survivorPostsLst = new ArrayList<>();
+        for (Post post : postsMap.values()) {
+            if (!post.getContent().equals(REMOVED)) {
+                survivorPostsLst.add(post);
+            }
+        }
+        return survivorPostsLst;
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.ofNullable(postsMap.get(id));
+        if (!postsMap.get(id).getContent().equals(REMOVED)) {
+            return Optional.ofNullable(postsMap.get(id));
+        } else {
+            throw new NotFoundException();
+        }
     }
 
     public Post save(Post post) {
         long specificID = post.getId();
         // если больше 0 и наличие
         if (specificID > 0 && postsMap.containsKey(specificID)) {
-            // заменяем
-            postsMap.replace(specificID, post);
+            // if (!post.getContent().equals(REMOVED)) {
+            if (!postsMap.get(specificID).getContent().equals(REMOVED)) {
+                // заменяем
+                postsMap.replace(specificID, post);
+            } else {
+                throw new NotFoundException();
+            }
         } else {
             long newIdPost = specificID == 0 ? postId.incrementAndGet() : specificID;
             post.setId(newIdPost);
@@ -44,6 +71,10 @@ public class PostRepositoryStubImpl implements PostRepository {
     }
 
     public void removeById(long id) {
-        postsMap.remove(id);
+        if (!postsMap.get(id).getContent().equals(REMOVED)) {
+            postsMap.get(id).setContent(REMOVED);
+        } else {
+            throw new NotFoundException();
+        }
     }
 }
