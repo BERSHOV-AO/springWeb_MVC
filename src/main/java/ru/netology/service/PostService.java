0bd95da0f6dc7de7837ecdf272ac7be7d478edc2
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-25 10:29:49 +0300

    Refactor PostService

--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -3,8 +3,10 @@ package ru.netology.service;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.netology.dto.PostDTO;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
@@ -12,6 +14,7 @@ import ru.netology.repository.PostRepository;
 //import net.javaguides.springboot.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -45,6 +48,10 @@ import java.util.stream.Collectors;
 @Service
 public class PostService {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class NotFoundException extends RuntimeException {
+    }
+
     private ModelMapper modelMapper;
     private static final String REMOVED = "removed";
 
@@ -65,57 +72,73 @@ public class PostService {
 
     //------------------------Конвертеры-------------------------
     // конвертер, mapper
-    private PostDTO convertEntityToDto(Post post) {
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
-        PostDTO postDTO = modelMapper.map(post, PostDTO.class);
-        return postDTO;
-    }
+//    private PostDTO convertEntityToDto(Post post) {
+//        modelMapper.getConfiguration()
+//                .setMatchingStrategy(MatchingStrategies.LOOSE);
+//        PostDTO postDTO = modelMapper.map(post, PostDTO.class);
+//        return postDTO;
+//    }
+//
+//    private Post convertDtoToEntity(PostDTO dto) {
+//        modelMapper.getConfiguration()
+//                .setMatchingStrategy(MatchingStrategies.LOOSE);
+//        Post post = modelMapper.map(dto, Post.class);
+//        return post;
+//    }
+
 
     private Post convertDtoToEntity(PostDTO dto) {
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
-        Post post = modelMapper.map(dto, Post.class);
-        return post;
+        // преобразование полей DTO в соответствующие поля сущности
+        var entity = new Post();
+        entity.setId(dto.getId());
+        entity.setContent(dto.getContent());
+        entity.setRemoved(dto.isRemoved());
+        return entity;
     }
 
-
-//    private Post convertDtoToEntity(PostDTO dto) {
-//        // преобразование полей DTO в соответствующие поля сущности
-//        var entity = new Post();
-//        entity.setId(dto.getId());
-//        entity.setContent(dto.getContent());
-//        entity.setRemoved(dto.isRemoved());
-//        return entity;
-//    }
-//
-//    private PostDTO convertEntityToDto(Post entity) {
-//        // преобразование полей сущности в соответствующие поля DTO
-//        var dto = new PostDTO();
-//        dto.setId(entity.getId());
-//        dto.setContent(entity.getContent());
-//        dto.setRemoved(entity.isRemoved());
-//        // другие поля...
-//
-//        return dto;
-//    }
-
-
+    private PostDTO convertEntityToDto(Post entity) {
+        // преобразование полей сущности в соответствующие поля DTO
+        var dto = new PostDTO();
+        dto.setId(entity.getId());
+        dto.setContent(entity.getContent());
+        dto.setRemoved(entity.isRemoved());
+        return dto;
+    }
     //-----------------------------------------------------------
 
     public PostDTO getById(long id) {
-        return convertEntityToDto(repository.getById(id).orElseThrow(NotFoundException::new));
+        boolean flagRemover = repository.getById(id).stream()
+                .map(Post::isRemoved)
+                .anyMatch(Boolean::booleanValue);
+
+        if (!flagRemover) {
+            return convertEntityToDto(repository.getById(id).orElseThrow(NotFoundException::new));
+        } else {
+            throw new NotFoundException();
+        }
     }
 
     // entity - сущность которая храниться в базе
     public PostDTO save(PostDTO dto) {
         var entity = convertDtoToEntity(dto);
-      //  System.out.println("entity: " + entity);
+        //  System.out.println("entity: " + entity);
         var savedEntity = repository.save(entity);
         return convertEntityToDto(savedEntity);
     }
 
+
+//    Optional<PostDTO> modifiedDTO = dtoList.stream()
+//            .peek(dto -> {
+//                dto.setTitle("Modified Title");
+//                dto.setContent("Modified Content");
+//            })
+//            .findFirst();
+
+
     public void removeById(@PathVariable Long id) {
-        repository.removeById(id);
+        // меняем
+        Optional<Post> firstObject = repository.getById(id).stream()
+                .peek(o -> o.setRemoved(true)).findFirst();
+        repository.save(firstObject.get());
     }
 }
